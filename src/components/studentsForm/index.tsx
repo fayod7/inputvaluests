4b597721f9d698647380ae1e48c35e442da087fd
fayod7 <fayodnv@gmail.com>
2025-08-11 16:59:07 +0500

    7

--- src/components/studentsForm/index.tsx
+++ src/components/studentsForm/index.tsx
@@ -18,15 +18,20 @@ const StudentsForm = () => {
     e.preventDefault()
   }
   const handleChange = (e: ChangeEvent<HTMLInputElement>) =>{
-    e.target
+    const {name, value} = e.target
+    setFormData(prev => ({
+      ...prev, [name]: value
+    }))
+    console.log(name, value);
+    
   }
   return (
     <div className="flex justify-center w-[70%]">
       <form onSubmit={handleSubmit} className='bg-white p-5 rounded-[10px] shadow-md flex flex-col gap-3'>
-         <input onChange={handleChange} name='name' required type="text" placeholder='name' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
-         <input onChange={handleChange} name='age' required type="text" placeholder='age' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
-         <input onChange={handleChange} name='address' required type="text" placeholder='address' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
-         <input onChange={handleChange} name='group' required type="text" placeholder='group' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
+         <input value={formData.name} onChange={handleChange} name='name' required type="text" placeholder='name' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
+         <input value={formData.age} onChange={handleChange} name='age' required type="text" placeholder='age' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
+         <input value={formData.address} onChange={handleChange} name='address' required type="text" placeholder='address' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
+         <input value={formData.group} onChange={handleChange} name='group' required type="text" placeholder='group' className='py-[8px] indent-3 focus:border-blue-500 border-[1px] border-slate-300 outline-none shadow-md text-[18px] rounded-[5px]'/>
          <button className='bg-slate-600 text-white text-[18px] px-[50px] py-[10px] mt-4 rounded-[7px] duration-200 hover:opacity-65 hover:cursor-pointer inline' type='submit'>submit</button>
       </form>
     </div>
